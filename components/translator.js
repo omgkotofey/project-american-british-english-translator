1a23aa46f48ba08970c025b47eba420d6c7267ef
OMG Kotofey <omgkotofey@gmail.com>
2022-05-18 21:52:13 +0000

    Fixed implementation after fcc tests

--- components/translator.js
+++ components/translator.js
@@ -3,7 +3,99 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+const AMERICAN_TO_BRITISH = 'american-to-british';
+const BRITISH_TO_AMERICAN = 'british-to-american'
+const alowedLocales = [AMERICAN_TO_BRITISH, BRITISH_TO_AMERICAN];
+
 class Translator {
+
+  _flipObject(obj) {
+    return Object.fromEntries(Object.entries(obj).map(a => a.reverse()))
+  }
+
+  _validateInput(text, locale) {
+    if (text == undefined || locale == undefined) {
+      throw new Error('Required field(s) missing');
+    }
+
+    if (!text) {
+      throw new Error('No text to translate');
+    }
+
+    if (!locale || !alowedLocales.includes(locale)) {
+      throw new Error('Invalid value for locale field');
+    }
+  }
+
+  _getLocaleTimeDelimeter(locale) {
+    return locale == AMERICAN_TO_BRITISH ? '.' : ':';
+  }
+
+  _checkWordIsTime(word, locale) {
+    const regexp = new RegExp(`[0-9]{2}[\.:][0-9]{2}`);
+    
+    return regexp.test(word);
+  }
+
+  _capitalizeFirstLetter(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  translate(text, locale) {
+    this._validateInput(text, locale);
+    let vocabulary = {
+      ...americanToBritishSpelling,
+      ...americanToBritishTitles
+    };
+
+    const textWords = text.split(' ');
+    if (locale == AMERICAN_TO_BRITISH) {
+      vocabulary = {
+        ...vocabulary,
+        ...americanOnly
+      };      
+    } else {
+      vocabulary = {
+        ...this._flipObject(vocabulary),
+        ...britishOnly
+      }
+    }
+    
+    let translatedWords = [];
+    let processedWords = [];
+    
+    textWords.forEach((word, index) => {
+      const normalizedWord = word.toLowerCase();
+      let translatedWord;
+
+      if (this._checkWordIsTime(word)) {
+        translatedWord = word.replace(/[.:]/, this._getLocaleTimeDelimeter(locale));
+      }
+      
+      if (vocabulary[normalizedWord]) {
+        translatedWord = vocabulary[normalizedWord];
+      };
+
+      if (translatedWord) {
+        if (index === 0) {
+          // capitalize firts letter by default
+          translatedWord = this._capitalizeFirstLetter(translatedWord);
+        }
+        // push translated word to related array
+        translatedWords.push(translatedWord);
+      }
+
+      // attach word to the end of the result
+      processedWords.push(translatedWord ? translatedWord : word);
+    })
+
+    return {
+      text: text,
+      locale: locale,
+      translatedWords: translatedWords,
+      translation: processedWords.join(' ')
+    };
+  }
 
 }
 
--- routes/api.js
+++ routes/api.js
@@ -6,8 +6,45 @@ module.exports = function (app) {
   
   const translator = new Translator();
 
+  const wrapWordWithHightligntTag = (word) => {
+    return `<span class="highlight">${word}</span>`
+  }
+
+  const highLightTranslation = (translationResult) => {
+    return translationResult.translation.split(' ').map(word => {
+      if (translationResult.translatedWords.includes(word)) {
+        return wrapWordWithHightligntTag(word);
+      }
+
+      return word;
+    }).join(' ');
+  }
+  
   app.route('/api/translate')
     .post((req, res) => {
+      const {locale, text} = req.body;
+      let result, translation;
       
+      try {
+        let translationResult = translator.translate(
+          text,
+          locale
+        );
+
+        if (translationResult.translation == text) {
+          translation = 'Everything looks good to me!';
+        } else {
+          translation = highLightTranslation(translationResult);
+        }
+        
+        result = {
+          text: translationResult.text,
+          translation: translation
+        }
+      } catch (err) {
+        result = {error: err.message}
+      }
+
+      return res.status(200).json(result);
     });
 };
